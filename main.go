c1034122f459f4bf23015a5c225bac2a2eeb110b
minseoi <minseoi.dev@gmail.com>
2022-12-30 12:05:44 +0900

    get-AllMemo

--- main.go
+++ main.go
@@ -20,6 +20,21 @@ type LoginResponseJson struct {
 	Token  *string      `json:"token,omitempty"`
 }
 
+type MemoListInfo struct {
+	MemoId int    `json:"memoId"`
+	Title  string `json:"title"`
+}
+
+type GetAllMemoListRequestJson struct {
+	LoginId string `json:"loginId"`
+	Token   string `json:"token"`
+}
+
+type GetAllMemoListResponseJson struct {
+	Error    *ErrorObject    `json:"error,omitempty"`
+	MemoList *[]MemoListInfo `json:"memoList,omitempty"`
+}
+
 func mainRounter(w http.ResponseWriter, r *http.Request) {
 	url := r.URL.Path[14:]
 
@@ -28,6 +43,8 @@ func mainRounter(w http.ResponseWriter, r *http.Request) {
 		http.NotFound(w, r)
 	case "/login":
 		login(w, r)
+	case "/all-memos":
+		getAllMemoList(w, r)
 	}
 }
 
@@ -55,7 +72,31 @@ func login(w http.ResponseWriter, r *http.Request) {
 			_token := "temp-token"
 			Response.Token = &_token
 		}
-		data, _ := json.Marshal(Response)
+		data, _ := json.MarshalIndent(Response, "", "    ")
+		fmt.Fprintf(w, string(data))
+	}
+}
+
+func getAllMemoList(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	default:
+		http.NotFound(w, r)
+
+	case http.MethodGet:
+		w.Header().Add("content-type", "application/json")
+		w.WriteHeader(http.StatusOK)
+
+		var Response = GetAllMemoListResponseJson{}
+		//test response
+		countOfMemos := 3
+		var newMemoList = make([]MemoListInfo, countOfMemos)
+		for i := 0; i < countOfMemos; i++ {
+			newMemoList[i].MemoId = i
+			newMemoList[i].Title = "memo-title"
+		}
+		Response.MemoList = &newMemoList
+
+		data, _ := json.MarshalIndent(Response, "", "    ")
 		fmt.Fprintf(w, string(data))
 	}
 }
@@ -64,8 +105,14 @@ func healthCheck(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 }
 
+func indexPage(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Welcome ChoKoSpace Server")
+}
+
 func main() {
 	mux := http.NewServeMux()
+	mux.HandleFunc("/", indexPage)
 	mux.HandleFunc("/status", healthCheck)
 	mux.HandleFunc("/api/chokomemo/", mainRounter)
 	http.ListenAndServe(":3901", mux)
