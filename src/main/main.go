8819409de55f471447c761354768e24eac5aa798
minseoi <minseoi.dev@gmail.com>
2023-01-22 21:06:11 +0900

    임시 cors해결

--- src/main/main.go
+++ src/main/main.go
@@ -12,6 +12,7 @@ func enableCors(w *http.ResponseWriter) {
 	(*w).Header().Set("Access-Control-Allow-Origin", "http://localhost")
 	(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 	(*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+	(*w).WriteHeader(http.StatusOK)
 }
 
 func apiRounter(w http.ResponseWriter, r *http.Request) {
